3be859fccf43ae15cca4d4ddda580775e48db97e
Spencer Brown <git@spencerwebservices.com>
2020-04-18 16:51:16 -0700

    Added curry function so that driver video can be easily re-used

--- demo.py
+++ demo.py
@@ -77,6 +77,39 @@ def make_animation(source_image, driving_video, generator, kp_detector, relative
             predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
     return predictions
 
+def make_animator_function_from_video(driving_video, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False):
+    with torch.no_grad():
+        predictions = []
+        
+        driving = torch.tensor(np.array(driving_video)[np.newaxis].astype(np.float32)).permute(0, 4, 1, 2, 3)
+        kp_driving_initial = kp_detector(driving[:, :, 0])
+
+        key_points_arr = []
+        num_of_frames = range(driving.shape[2])
+        for frame_idx in tqdm(num_of_frames):
+            driving_frame = driving[:, :, frame_idx]
+            if not cpu:
+                driving_frame = driving_frame.cuda()
+            key_points_arr.append(kp_detector(driving_frame))
+
+    def finish_animation(source_image):
+        source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
+        if not cpu:
+            source = source.cuda()
+        kp_source = kp_detector(source)
+
+        for frame_idx in tqdm(num_of_frames):
+            kp_norm = normalize_kp(kp_source=kp_source, kp_driving=key_points_arr[frame_idx],
+                                   kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
+                                   use_relative_jacobian=relative, adapt_movement_scale=adapt_movement_scale)
+            out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
+
+            predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
+
+        return predictions
+    
+    return finish_animation
+
 def find_best_frame(source, driving, cpu=False):
     import face_alignment
 
